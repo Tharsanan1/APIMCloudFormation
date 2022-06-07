a3b8c22682ddacaf20b0248a99ba78f76d8c0dd8
Tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2022-06-07 13:12:07 +0530

    test

--- cleanup.sh
+++ cleanup.sh
@@ -6,9 +6,5 @@ echo "Scaling node group instances to zero."
 eksctl scale nodegroup --region ${EKS_CLUSTER_REGION} --cluster ${EKS_CLUSTER_NAME} --name ng-1 --nodes=0 || true
 echo "Uninstalling APIM in cluster."
 helm uninstall "${product_name}" || true
-echo "Deleting RDS database."
-aws cloudformation delete-stack --region ${EKS_CLUSTER_REGION} --stack-name ${RDS_STACK_NAME} ; aws cloudformation wait stack-delete-complete --region ${EKS_CLUSTER_REGION} --stack-name apim-rds-stack || true
-
-
 
 cd "$workingdir"
