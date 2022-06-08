e22118221ff021664f0e9c1f366c658d3f3d6dc0
Tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2022-06-08 09:17:19 +0530

    test

--- cleanup.sh
+++ cleanup.sh
@@ -2,8 +2,7 @@ workingdir=$(pwd)
 reldir=`dirname $0`
 cd $reldir
 
-echo "Scaling node group instances to zero."
-eksctl scale nodegroup --region ${EKS_CLUSTER_REGION} --cluster ${EKS_CLUSTER_NAME} --name ng-1 --nodes=0 || true
+
 echo "Uninstalling APIM in cluster."
 helm uninstall "${product_name}" || true
 
