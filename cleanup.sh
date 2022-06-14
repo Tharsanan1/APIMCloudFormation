893cea76c3fcb5ce0748d6fdd8daee4a23598185
Tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2022-06-14 10:37:42 +0530

    Add namespace param to helm uninstall command

--- cleanup.sh
+++ cleanup.sh
@@ -3,7 +3,7 @@ reldir=`dirname $0`
 cd $reldir
 
 echo "Uninstalling APIM in cluster."
-helm uninstall "${product_name}" || true
+helm uninstall "${product_name}" -n="${kubernetes_namespace}" || true
 
 echo "Delete fargate profile."
 eksctl delete fargateprofile  --name "${product_name}-fargate-profile" --cluster "${EKS_CLUSTER_NAME}" --region ${EKS_CLUSTER_REGION}
